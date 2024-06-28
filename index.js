56deed5f70e30a795c8b54b5abf62eae2ad2b38f
Abdikhafar <abdikhafarissack@gmail.com>
2024-06-28 23:27:45 +0300

    class function

---
+++ index.js
@@ -0,0 +1,30 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+function myCallback() {
+    console.log("Callback function has been called!");
+}
+
+receivesAFunction(myCallback);
+
+
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("This is a named function!");
+    }
+    return namedFunction;
+}
+
+const myNamedFunction = returnsANamedFunction();
+myNamedFunction(); // This will log "This is a named function!" to the console.
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This is an anonymous function!");
+    };
+}
+
+const myAnonymousFunction = returnsAnAnonymousFunction();
+myAnonymousFunction();
